2dd21fc584c39cea30234b28a01e1314b3d19f06
daryatalantseva <d.talantseva@gmail.com>
2022-08-31 09:46:08 +0300

    Задача 66

---
+++ sem9_66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = InputNumbers("Введите значение начало диапазона ");
+int n = InputNumbers("Введите значение конца диапазона ");
+int x = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = x;
+}
+
+VivodSum(m, n, x = 0);
+
+void VivodSum(int m, int n, int sum)
+{
+  sum = sum + n;
+  if (n <= m)
+  {
+    Console.Write("Сумма элементов= " + sum);
+    return;
+  }
+  VivodSum(m, n - 1, sum);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int y = Convert.ToInt32(Console.ReadLine());
+  return y;
+}
